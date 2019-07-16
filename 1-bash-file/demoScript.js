f9c4ed10a9453d8f90a3e3acdee4f43cdcec4d97
AndyL <missedmemo@gmail.com>
2019-07-15 22:11:18 -0700

    process file contents via stdin

--- 1-bash-file/demoScript.js
+++ 1-bash-file/demoScript.js
@@ -3,42 +3,44 @@
 
 const path = require('path')
 const fs = require('fs')
+const getStdIn = require('get-stdin')
+const util = require('util')
 
 const args = require("minimist")(process.argv.slice(2), {
-  boolean: ["help"],
+  boolean: ["help", "in"],
   string: ["file"]
 })
 
 if ( args.help ) {
   printHelp()
 }
+else if (args.in || args._.includes('-')) {
+  getStdIn().then( processData ).catch( showError )
+}
 else if ( args.file ) {
-  processFile( path.resolve(args.file) )
-}
-else {
-  error("incorrect usage!", true )
-}
-
-//************************************
-
-function processFile( pathAndFile ) {
-  /*
-  const buffer = fs.readFileSync(pathAndFile)
-  console.log(buffer)
-  process.stdout.write(buffer)
-  */
+  const pathAndFile = path.resolve(args.file)
 
   fs.readFile( pathAndFile, (err,data) => {
     if(err) {
       console.log( err.toString() )
     }
     else {
-      process.stdout.write(data)
+      processData(data)
     }
   })
 }
+else {
+  showError("incorrect usage!", true )
+}
 
-function error(message, showHelp) {
+//************************************
+
+function processData( data ) {
+  const upCased = data.toString().toUpperCase()
+  process.stdout.write(upCased)
+}
+
+function showError(message, showHelp) {
   console.error( `\nERROR: ${message}` )
   if ( showHelp ) {
     printHelp()
@@ -50,12 +52,15 @@ function printHelp() {
   const helpText = `
     Usage:
 
-      ./demoScript --file={FILENAME}
+      node demoScript --file={FILENAME}
+      ./demoScript.js --file={FILENAME}
+      cat {FILENAME} | ./demoScript.js --in
     
     Options:
       
       --help                print this help
       --file={FILENAME}     process the file
+      --in, -               process from stdin
   `
 
   console.log('\n' + helpText)
